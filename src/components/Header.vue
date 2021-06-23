62d2fc7cf3f5986a6b0bdd83e69a4130246268d5
TuziMoe <a@tuzi.moe>
2021-06-23 18:52:08 +1200

    chord: init vuesax

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="center example-nav">
+    <vs-navbar not-line center-collapsed v-model="active">
+      <template #left>
+        <img
+          src="@/assets/RSS3_Scan.svg"
+          style="
+              height: 7vh;
+          "
+          alt=""
+        />
+      </template>
+      <vs-navbar-item :active="active == 'home'" id="home">
+        Home
+      </vs-navbar-item>
+      <vs-navbar-item :active="active == 'address'" id="address">
+        Address
+      </vs-navbar-item>
+      <vs-navbar-item :active="active == 'resources'" id="resources">
+        Resources
+      </vs-navbar-item>
+      <vs-navbar-item :active="active == 'about'" id="about">
+        About
+      </vs-navbar-item>
+      <template #right>
+        <!-- <vs-button flat></vs-button> | -->
+        <vs-button gradient>Sign In</vs-button>
+      </template>
+    </vs-navbar>
+  </div>
+</template>
+<script>
+export default {
+  name: "Header",
+  data:() => ({
+      active: 'home'
+    })
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from "vue";
+import Vuesax from 'vuesax'
 import App from "./App.vue";
 import router from "./router";
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+import '@/assets/css/theme.css' 
 
 Vue.config.productionTip = false;
+Vue.use(Vuesax, {
+  colors: {
+    primary:'#87EB00',
+    success:'rgb(23, 201, 100)',
+    danger:'rgb(242, 19, 93)',
+    warning:'rgb(255, 130, 0)',
+    dark:'rgb(36, 33, 69)'
+  }
+})
 
 new Vue({
   router,
