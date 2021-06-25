33e5eeb421ba927b7490496b01c2da477fd1d53a
Rorical <hash_548@163.com>
2021-06-25 09:37:51 +0800

    update item page

---
+++ src/components/common/Date.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="date-component">
+    <vs-button border color="rgb(59,222,200)" :active="true">
+      <slot></slot>
+    </vs-button>
+    <div class="date-show">
+      <vs-button border color="rgb(59,222,200)" :active="true">
+        {{ year }}
+      </vs-button>
+      <vs-button border color="rgb(59,222,200)" :active="true">
+        {{ month }}
+      </vs-button>
+      <vs-button border color="rgb(59,222,200)" :active="true">
+        {{ day }}
+      </vs-button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Date",
+    props: {
+      date: {
+        type: String
+      }
+    },
+    data: () => ({
+      year: "####",
+      month: "##",
+      day: "##"
+    }),
+    watch: {
+      "date": "parse"
+    },
+    mounted() {
+      this.parse()
+    },
+    methods: {
+      parse() {
+        let date = this.date.split("T")[0]
+        this.year = date.split("-")[0]
+        this.month = date.split("-")[1]
+        this.day = date.split("-")[2]
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .date-component {
+    display: flex;
+    align-items: center;
+
+    .date-show {
+      display: -webkit-box;
+    }
+  }
+</style>
--- src/views/Item.vue
+++ src/views/Item.vue
@@ -1,5 +1,20 @@
 <template>
-  <section class="items-section">
+  <section class="items-section" v-if="item">
+    <h1 class="title"> {{ item.title|noNull("No Title") }} </h1>
+    <div class="summary">
+      <div class="author">
+        Authors:
+        <div class="author-list">
+          <vs-button transparent v-for="author in item.authors" :key="author"
+            @click="$router.push({'name': 'Address', params: {'addr': author}})">{{ author }}</vs-button>
+        </div>
+      </div>
+      <vs-alert>
+        {{ item.summary|noNull("Empty Summary") }}
+      </vs-alert>
+      
+      
+    </div>
     
   </section>
 </template>
@@ -8,16 +23,86 @@
   export default {
     name: "Item",
     data: () => ({
-      
+      item: null,
+      loading: null,
+      address: "",
+      id: 0
     }),
     created() {
-      
+      this.loading = this.$vs.loading();
+      this.address = this.$route.params.addr;
+      this.id = this.$route.params.item;
     },
     mounted() {
-      
+      this.load()
+    },
+    methods: {
+      load() {
+        this.axios.get("https://hub.rss3.io/" + this.address).then((response) => {
+          this.item = response.data.items.find(item => {
+            return this.parseItemId(item.id) == this.id
+          });
+          this.loading.close();
+        }).catch((err) => {
+          console.log(err)
+          this.error("Address Error", "Please Check Your Input Again!")
+          this.loading.close();
+          this.$router.push({
+            "name": "Home"
+          })
+        })
+      },
+      parseItemId(id) {
+        const splited = id.split('-');
+        return splited[2] !== undefined ? parseInt(splited[2]) : Infinity
+      }
+    },
+    filters: {
+      noNull(data, text) {
+        if (data) return data;
+        return text
+      }
     }
   }
 </script>
 
-<style>
+<style lang="scss">
+  .items-section {
+    max-width: 70rem;
+    margin: 0 auto;
+    padding: 2rem 2.5rem;
+
+    .title {
+      margin: {
+        bottom: 1.4rem;
+      }
+    }
+
+    .summary {
+      max-width: 40rem;
+      margin: auto;
+      height: auto !important;
+
+      .author {
+        display: flex;
+        align-items: center;
+
+        .author-list {
+          display: -webkit-box;
+          overflow-x: auto;
+        }
+      }
+
+      .vs-alert__content {
+        display: flex;
+        text-align: start;
+        min-height: auto !important;
+
+        .vs-alert__content__text {
+          width: 100%;
+          color: black;
+        }
+      }
+    }
+  }
 </style>
